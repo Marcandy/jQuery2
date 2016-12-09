4685262cae64ea4ca0d34850c5e5af00a54b56c7
marcandy <marcandy.nj@gmail.com>
2016-12-08 22:03:04 -0700

    review

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -2,16 +2,51 @@ $(document).ready(function() {
 
   var listo = [];
 
-  function Tasks(task) {
+  $('#newTaskForm').hide();
+  $('#add-todo').click(function () {
+    $('#newTaskForm').fadeToggle('fast', 'linear');//equivalent to show and hide
+  })
+
+  $('#cancel').on('click', function(e) {
+    e.preventDefault();
+    $('#newTaskForm').fadeToggle('fast', 'linear');
+  })
+
+  function Task(task) {
     this.task = task;
     this.id = 'new';
   }
 
   function addTask(task) {
     if(task) {
-      var newTask = new Tasks(task);
-      return listo.push(newTask);
+      var newTask = new Task(task);
+      listo.push(newTask);
+
+      $('#newItemInput').val("");
+
+      $('#newList').append(
+                        '<a href="#finish" class="" id="item">' +
+                        '<li class="list-group-item">' +
+                        '<h3>' + newTask.task + '</h3>'+
+                        '<span class="arrow pull-right">' +
+                        '<i class="glyphicon glyphicon-arrow-right">' +
+                        '</span>' +
+                        '</li>' +
+                        '</a>'
+                      );
     }
+    console.log(newTask);
+    $('#newTaskForm').slideToggle('fast', 'linear')
   }
+
+
+
+  $('#saveNewItem').on('click', function(e) {
+    e.preventDefault();
+    var task = $('#newItemInput').val().trim();
+    addTask(task);
+  })
+
+
 
 });
